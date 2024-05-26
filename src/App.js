270ec3832624ea982c52d8f4517a8c76ee9b8ec9
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2024-05-26 10:12:09 +0300

    Final review

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
-
 const messages = [
-  'Learn React ⚛️',
+  'Learn React ⚛',
   'Apply for jobs 💼',
   'Invest your new income 🤑',
 ];
@@ -9,15 +8,14 @@ export default function App() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
-    if (step > 1) setStep(s => s - 1);
+    if (step > 1) setStep(currStep => currStep - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(s => s + 1);
+    if (step < 3) setStep(currStep => currStep + 1);
   }
-
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(is => !is)}>
+      <button className="close" onClick={() => setIsOpen(isOpen => !isOpen)}>
         &times;
       </button>
       {isOpen && (
@@ -30,17 +28,16 @@ export default function App() {
           <p className="message">
             Step {step} : {messages[step - 1]}
           </p>
-          ;
           <div className="buttons">
             <button
+              style={{ backgroundColor: '#7950F2', color: '#FFF' }}
               onClick={handlePrevious}
-              style={{ backgroundColor: '#7950F2', color: '#fff' }}
             >
               Previous
             </button>
             <button
+              style={{ backgroundColor: '#7950F2', color: '#FFF' }}
               onClick={handleNext}
-              style={{ backgroundColor: '#7950F2', color: '#fff' }}
             >
               Next
             </button>
