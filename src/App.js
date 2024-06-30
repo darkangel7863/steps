71f075e78e234de5788c9dc0503f6abb9ec57918
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2024-06-30 11:11:44 +0300

    Extra feature

--- src/App.js
+++ src/App.js
@@ -29,9 +29,7 @@ function Steps() {
             <div className={step >= 2 ? 'active' : ''}>2</div>
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950F2" onClick={handlePrevious}>
               <span>👈</span> Previous
@@ -43,6 +41,13 @@ function Steps() {
         </div>
       )}
     </>
+  );
+}
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3> {children}
+    </div>
   );
 }
 function Button({ textColor, bgColor, onClick, children }) {
