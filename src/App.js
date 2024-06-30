cad83f9f6d6816c3da96ece3f2c1826c28388e26
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2024-06-30 11:02:00 +0300

    Rebuilding the component

--- src/App.js
+++ src/App.js
@@ -33,21 +33,25 @@ function Steps() {
             Step {step}: {messages[step - 1]}
           </p>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: '#7950F2', color: '#fff' }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: '#7950F2', color: '#fff' }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950F2" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+            <Button textColor="#fff" bgColor="#7950F2" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
